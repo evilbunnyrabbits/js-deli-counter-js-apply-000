98517c4b99e000ec7126862a244c9534275a2ccb
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-05-11 15:13:07 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,31 @@
+let bensWaitingList = []
+
+//adds a person to the line
+function takeANumber(line, name) {
+    line.push(name);
+    let position = line.length;
+    return `Welcome, ${name}. You are number ${position} in line.`
+}
+
+//calls now serving person, shifts line.
+function nowServing(line) {
+    let calling = line[0];
+    if (line.length === 0) {
+        return 'There is nobody waiting to be served!';
+    } else {
+        line.shift();
+    } return `Currently serving ${calling}.`;
+}
+
+//checks who's on the line.
+function currentLine(line) {
+    let checkLine = [];
+    if (line.length === 0) {
+        return 'The line is currently empty.';
+    } else {
+        for (i=0; i<line.length; i++) {
+            let j = (i + 1) + '. '
+            checkLine.push(j + line[i])
+        } return checkLine;
+    }
+}
